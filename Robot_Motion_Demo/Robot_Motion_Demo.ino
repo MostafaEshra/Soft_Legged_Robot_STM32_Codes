f6309539c4930171b0ff4779e331331b38312f66
Mostafa Eshra <125136185+MostafaEshra@users.noreply.github.com>
2025-01-17 18:37:18 +0200

    Added Demo for first 1 leg movement

--- Robot_Motion_Demo/Robot_Motion_Demo.ino
+++ Robot_Motion_Demo/Robot_Motion_Demo.ino
@@ -43,8 +43,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Loop");
-
-  Backward_2Legs();
+ Backward_2Legs();
 }
 
 
@@ -96,38 +95,73 @@ void Servo_2_Angle(char servo,int angle) {
 }
 
 void Reset() {
-    for (int i = 0; i < 9; i++) {
-      Servo_2_Angle(i, 0);
-    }
+//    for (int i = 0; i < 9; i++) {
+    //  Servo_2_Angle(i, 0);
+  // Servo_2_Angle(servo1, 45);
+  // Servo_2_Angle(servo2, 45);
+  // Servo_2_Angle(servo3, 67.5);
+  // Servo_2_Angle(servo4, 45);
+  // Servo_2_Angle(servo5, 67.5);
+  // Servo_2_Angle(servo6, 45);
+  // Servo_2_Angle(servo7, 67.5);
+  // Servo_2_Angle(servo8, 45);
+  // Servo_2_Angle(servo9, 45);
+//    }
+  driver.writeMicroseconds(0, 400);
+  driver.writeMicroseconds(1, 400);
+  driver.writeMicroseconds(2, 800);
+  driver.writeMicroseconds(3, 400);
+  driver.writeMicroseconds(4, 600);
+  driver.writeMicroseconds(5, 400);
+  driver.writeMicroseconds(6, 600);
+  driver.writeMicroseconds(7, 400);
+  driver.writeMicroseconds(8, 400);
     delay(1500);
 }
 
 void Backward_2Legs() {
-  // Servo_2_Angle(servo2, 35);
-  // Servo_2_Angle(servo3, 35);
-  // Servo_2_Angle(servo8, 20);
-  // Servo_2_Angle(servo6, 20);
-
-  Servo_2_Angle(servo9, 170);
-  Servo_2_Angle(servo7, 170);
-  Servo_2_Angle(servo4, 170);
-  Servo_2_Angle(servo5, 170);
-  Servo_2_Angle(servo1, 170);
-  delay(1500);
-  Servo_2_Angle(servo9, 0);
-  Servo_2_Angle(servo7, 0);
-  Servo_2_Angle(servo4, 0);
-  Servo_2_Angle(servo5, 0);
-  delay(1500);
-  Servo_2_Angle(servo6, 170);
-  Servo_2_Angle(servo8, 170);
-  delay(1500);
-  Servo_2_Angle(servo1, 0);
-  delay(1500);
-  Servo_2_Angle(servo2, 170);
-  Servo_2_Angle(servo3, 50);
-  delay(1500);
+  // driver.writeMicroseconds(0, 2400);
+  // driver.writeMicroseconds(1, 400);
+  // driver.writeMicroseconds(2, 1000);
+  // driver.writeMicroseconds(3, 400);
+  // driver.writeMicroseconds(4, 600);
+  // driver.writeMicroseconds(5, 400);
+  // driver.writeMicroseconds(6, 600);
+  // driver.writeMicroseconds(7, 400);
+  // driver.writeMicroseconds(8, 400);
+  // delay(500);
+  //trail 1 shohdy 14-1-2025
+  // driver.writeMicroseconds(0, 2700);
+  // driver.writeMicroseconds(1, 400);
+  // driver.writeMicroseconds(2, 800);
+  // driver.writeMicroseconds(3, 400);
+  // driver.writeMicroseconds(4, 600);
+  // driver.writeMicroseconds(5, 400);
+  // driver.writeMicroseconds(6, 600);
+  // driver.writeMicroseconds(7, 400);
+  // driver.writeMicroseconds(8, 400);
+  // delay(1500);
+  //trail ووقع 14-1-2025
+  // driver.writeMicroseconds(0, 2700);
+  // driver.writeMicroseconds(1, 400);
+  // driver.writeMicroseconds(2, 800);
+  // driver.writeMicroseconds(3, 400);
+  // driver.writeMicroseconds(4, 600);
+  // driver.writeMicroseconds(5, 400);
+  // driver.writeMicroseconds(6, 600);
+  // driver.writeMicroseconds(7, 400);
+  // driver.writeMicroseconds(8, 400);
+  // delay(1500);
   Reset();
+    // Servo_2_Angle(servo1, 160);
+  // Servo_2_Angle(servo2, 45);
+  // Servo_2_Angle(servo3, 110);
+  // Servo_2_Angle(servo4, 45);
+  // Servo_2_Angle(servo5, 67.5);
+  // Servo_2_Angle(servo6, 45);
+  // Servo_2_Angle(servo7, 67.5);
+  // Servo_2_Angle(servo8, 45);
+  // Servo_2_Angle(servo9, 45);
 }
 
 void Move_Lift(){
